56b989cee0811d1f061af6df92a5ad1a0e6ec4ec
emzamike1997-lab <emzamike1997@gmail.com>
2026-03-16 09:40:29 +0100

    JAVAS: Add src/pages/About.js

---
+++ src/pages/About.js
@@ -0,0 +1,16 @@
+```javascript
+import React from 'react';
+
+function About() {
+  return (
+    <div className="about">
+      <h1>About me</h1>
+      <p>I'm a photographer with over 5 years of experience.</p>
+    </div>
+  );
+}
+
+export default About;
+```
+
+###
