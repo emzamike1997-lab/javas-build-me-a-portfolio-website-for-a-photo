53f8e86af7e3e1974b519a18be807061b0662262
emzamike1997-lab <emzamike1997@gmail.com>
2026-03-16 09:40:42 +0100

    JAVAS: Add app/config.py

---
+++ app/config.py
@@ -0,0 +1,10 @@
+```python
+import os
+
+class Config:
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///portfolio.db'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'secret_key'
+```
+
+###
