6254d7039ffa3b52a5b3b717dfc755080c1db725
emzamike1997-lab <emzamike1997@gmail.com>
2026-03-16 09:40:31 +0100

    JAVAS: Add src/pages/Contact.js

---
+++ src/pages/Contact.js
@@ -0,0 +1,16 @@
+```javascript
+import React from 'react';
+
+function Contact() {
+  return (
+    <div className="contact">
+      <h1>Get in touch</h1>
+      <p>Email: <a href="mailto:example@example.com">example@example.com</a></p>
+    </div>
+  );
+}
+
+export default Contact;
+```
+
+###
