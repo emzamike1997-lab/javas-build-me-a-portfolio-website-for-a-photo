3fbc6275e47cf70932664357ff8766abe15dc311
emzamike1997-lab <emzamike1997@gmail.com>
2026-03-16 09:40:28 +0100

    JAVAS: Add src/pages/Home.js

---
+++ src/pages/Home.js
@@ -0,0 +1,16 @@
+```javascript
+import React from 'react';
+
+function Home() {
+  return (
+    <div className="home">
+      <h1>Welcome to my portfolio website!</h1>
+      <p>I'm a photographer with a passion for capturing life's moments.</p>
+    </div>
+  );
+}
+
+export default Home;
+```
+
+###
