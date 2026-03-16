2dabc959550bfd60219c73c6b6d80e0a92b81476
emzamike1997-lab <emzamike1997@gmail.com>
2026-03-16 10:25:24 +0100

    JAVAS: Add utils.py

---
+++ utils.py
@@ -0,0 +1,15 @@
+```python
+import logging
+from flask import Flask
+
+def configure_logger(app):
+    """Configure logging"""
+    app.logger.setLevel(logging.DEBUG)
+    handler = logging.StreamHandler()
+    handler.setLevel(logging.DEBUG)
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    handler.setFormatter(formatter)
+    app.logger.addHandler(handler)
+```
+
+###
