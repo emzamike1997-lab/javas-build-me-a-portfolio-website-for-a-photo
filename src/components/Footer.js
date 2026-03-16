0db48d264748dd3fdffcbf6598a5e2a66dd06ce3
emzamike1997-lab <emzamike1997@gmail.com>
2026-03-16 09:40:27 +0100

    JAVAS: Add src/components/Footer.js

---
+++ src/components/Footer.js
@@ -0,0 +1,15 @@
+```javascript
+import React from 'react';
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <p>&copy; 2024 Photographer Portfolio</p>
+    </footer>
+  );
+}
+
+export default Footer;
+```
+
+###
